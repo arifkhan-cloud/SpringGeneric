9a46ab80dac6b97601247289f384729e93c17c63
Arif Ikbal Khan <arif.ikbal@bcits.in>
2021-02-19 23:14:29 +0530

    our first ommit

---
+++ src/main/java/com/khan/BootTutorial/BootTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.khan.BootTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khan/BootTutorial/BootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khan.BootTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
